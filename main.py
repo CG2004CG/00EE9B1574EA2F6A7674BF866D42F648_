9169982e6e4ae6390972b2a574435f3540355bca
CG2004CG <144463444+CG2004CG@users.noreply.github.com>
2023-09-29 21:50:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
- # Python program to find the factorial of a number provided by the user.
-
-# change the value for a different result
-num = 7
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+#2.2 Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object.
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+# Call the play() method for each object
+batsman.play()
+bowler.play()
